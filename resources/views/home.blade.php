4b29158baf21315d1202c939877058e010f8d846
AisyaAnwarPutri <aisyanwarputri@gmail.com>
2023-06-06 14:19:09 +0700

    blade home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <div class="container" style="margin: 0 auto; width: 500px; height: 200px;">
+        <h1>Welcome to the Home Page!</h1>
+        <a href="{{ route('register') }}">Register</a>
+    </div>
+</body>
+</html>
